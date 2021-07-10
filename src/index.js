8a77a7c48e736f6eb1353e0baf3ce44c701ad45e
Harshith <harshithece7016@gmail.com>
2021-07-10 05:44:37 +0530

    feat: support images and svg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import App from './App';
+import App from "./App"
+import Logo from "./logo.svg"
 
 const root = document.getElementById("root");
 
-root.innerHTML = `<h1> Learn Web Tooling </h1>
+root.innerHTML = `<img src="${Logo}" alt="build-icon" />
+<h1> Learn Web Tooling </h1>
 ${App({name:"Harshith Venkatesh"})}
 `
--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,14 @@ const webpackConfig = {
                         presets:["@babel/preset-env"]
                     }
                 }
-            },{
+            },
+            {
                 test: /\.css/i,
                 use: ["style-loader","css-loader"]
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset"
             }
         ]
     },
