a0d0c8c8a2d8ced64df56785356b108c0879df80
Harshith <harshithece7016@gmail.com>
2021-07-10 15:41:43 +0530

    feat: code spitting mechanism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import _ from 'lodash'
 import "./App.css"
 
+
 const App = ({name}) => {
-    return `<h2>Welcome, ${name}</h2>`
+    return `
+    <h2>Welcome, ${_.join(["Welcome",name," "])}</h2>
+    <p> Build app using loadash</p>`
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpackConfig = {
     entry: path.resolve(__dirname,"src","index.js"),
     output: {
-        filename:"main.js",
+        filename:"[name].bundle.js",
         path: path.resolve(__dirname,"dist"),
         clean:true
     },
@@ -35,6 +35,18 @@ const webpackConfig = {
             template: path.resolve(__dirname,"src","index.html")
         })
     ],
+    optimization:{
+        splitChunks: {
+            cacheGroups: {
+                node_vendors: {
+                    name:"vendor",
+                    test:"/[\\/]node_modules[\\/]]/",
+                    chunks:"all",
+                    priority: 1
+                }
+            }
+        }
+    },
     mode:"production"
 }
 
