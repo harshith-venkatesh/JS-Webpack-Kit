6f9762ba401b65c130584227714921c1b316d9a4
Harshith <harshithece7016@gmail.com>
2021-07-11 20:00:03 +0530

    feat: features are added

--- src/index.js
+++ src/index.js
@@ -26,6 +26,19 @@ function init() {
                 <button id="click_about">Github</button>
             </div>
             <div id="home_page"></div>
+            <div class="features_tab">
+                <h2>Features</h2>
+                <ul class="list_features">
+                    <li>production mode with <strong>minified files</strong></li>
+                    <li>Use ES6 Syntax without worrying about browser support as <strong>babel</strong> is pre-configured</li>
+                    <li>Import <strong>CSS files into JS</strong> files directly. </li>
+                    <li>Import Images into JS files. The above build icon is an example of same.</li>
+                    <li><strong>Inline asset management</strong> for smaller sized assets (i.e they are added in the main JS file)</li>
+                    <li>Loadash is configured and a separate file is generated for its code. ( example of <strong>code-spliting</strong> for vendor code) </li>
+                    <li><strong>Caching</strong> is done by adding content-hash as part of file name. So only files whose contents change get a new hash, i.e a new name.</li>
+                    <li><strong>EsLint Module</strong> included.</li>
+                </ul>
+            </div>
         </div>
         `
 }
